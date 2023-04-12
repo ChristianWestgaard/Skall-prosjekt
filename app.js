0d22c161d30fa7f19aea784f0d50fbd5c50871a6
Christian Westgaard <westgaard04@gmail.com>
2023-04-12 23:52:33 +0200

    idkman

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
+
+
 const { MongoClient } = require("mongodb");
  
 // Replace the following with your Atlas connection string                                                                                                                                        
@@ -5,6 +28,7 @@ const url = "mongodb+srv://christianwestgaard04:ShellProject0904@cluster0.sxqcbo
 const client = new MongoClient(url);
 
 const dbName = "test";
+const DataB = "DataBase"
 
 async function run() {
     try {
@@ -36,3 +60,60 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+async function logInnFuction() {
+
+    // db.collection('Brukere').get().then((snapshot) => {
+    //   snapshot.docs.forEach(doc => {
+    //     console.log(doc.data());
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(DataB);
+        // Use the collection "people"
+        const col = db.collection("users");
+        // **__**__**__**__**__**__**__**__**
+        let userInfo ={
+            "username": document.getElementById("useri")
+        }
+
+        const q = await col.insertOne(userInfo);
+        const user = await col.findOne();
+        console.log(user);
+        
+        if (user("username") = document.getElementById("useri")){
+        document.getElementById("maintext") = user
+        }
+
+    }
+    finally{
+        await client.close();
+    }
+};
+  
+        // if ((UserInput === uName && UserPassword === uPassword) && uRole === 'Kunde') {
+        //   console.log("is this true?");
+        //   logInnState = true;
+        //   console.log(logInnState);
+        // }
+  
+        // if ((UserInput === uName && UserPassword === uPassword) && uRole === 'Admin') {
+        //   // window.location.replace = "https://www.vg.no";
+        //   console.log("is this true?");
+        //   logInnState = true;
+        //   admin = true
+        //   console.log(logInnState);
+        //   console.log(admin);
+        // }
+      
+  
+//       if (logInnState && !admin) {
+//         window.location.replace("http://127.0.0.1:5501/index.html")
+//       }
+//       if (logInnState && admin) {
+//         window.location.replace("http://127.0.0.1:5501/html/adminside.html")
+  
+//       }
+    
+//   }
+// };
